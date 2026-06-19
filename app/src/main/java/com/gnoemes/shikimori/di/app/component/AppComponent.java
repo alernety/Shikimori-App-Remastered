f78560ec48438dc72928c8b17ba3b25d4c0583ba
Ali Sha <alernety@gmail.com>
2026-06-19 17:29:56 +0300

    chore(di): remove empty Dagger modules and simplify component

--- app/src/main/java/com/gnoemes/shikimori/di/app/component/AppComponent.java
+++ app/src/main/java/com/gnoemes/shikimori/di/app/component/AppComponent.java
@@ -2,7 +2,6 @@ package com.gnoemes.shikimori.di.app.component;
 
 import com.gnoemes.shikimori.App;
 import com.gnoemes.shikimori.di.ActivityInjectionModule;
-import com.gnoemes.shikimori.di.ServiceInjectionModule;
 import com.gnoemes.shikimori.di.app.module.AppModule;
 import com.gnoemes.shikimori.di.app.module.ConverterModule;
 import com.gnoemes.shikimori.di.app.module.InteractorModule;
@@ -10,7 +9,6 @@ import com.gnoemes.shikimori.di.app.module.NavigationModule;
 import com.gnoemes.shikimori.di.app.module.RepositoryModule;
 import com.gnoemes.shikimori.di.app.module.UtilModule;
 import com.gnoemes.shikimori.di.app.module.local.DbModule;
-import com.gnoemes.shikimori.di.app.module.local.HistoryModule;
 import com.gnoemes.shikimori.di.app.module.local.SettingsModule;
 import com.gnoemes.shikimori.di.app.module.network.ApiModule;
 
@@ -20,10 +18,10 @@ import dagger.Component;
 import dagger.android.AndroidInjector;
 
 @Singleton
-@Component(modules = {AppModule.class, ApiModule.class, ServiceInjectionModule.class,
+@Component(modules = {AppModule.class, ApiModule.class,
         ActivityInjectionModule.class, NavigationModule.class,
         UtilModule.class, DbModule.class, SettingsModule.class,
-        HistoryModule.class, RepositoryModule.class, InteractorModule.class,
+        RepositoryModule.class, InteractorModule.class,
         ConverterModule.class})
 public interface AppComponent extends AndroidInjector<App> {
 
