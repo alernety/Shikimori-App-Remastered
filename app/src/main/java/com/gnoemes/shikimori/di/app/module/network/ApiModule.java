4b2e8d5ec9d29c4e5191c1fb5003e2b9d5bfe7a2
Ali Sha <alernety@gmail.com>
2026-07-01 01:31:16 +0300

    refactor(network): remove deprecated DocumentVideo web scraping module
    Remove DocumentVideoApi, DocumentVideoApi annotation, and DocumentVideoNetworkModule. Update ApiModule to remove DocumentVideo API bindings.

--- app/src/main/java/com/gnoemes/shikimori/di/app/module/network/ApiModule.java
+++ app/src/main/java/com/gnoemes/shikimori/di/app/module/network/ApiModule.java
@@ -4,7 +4,6 @@ import com.gnoemes.shikimori.data.network.AnimeApi;
 import com.gnoemes.shikimori.data.network.AuthApi;
 import com.gnoemes.shikimori.data.network.CalendarApi;
 import com.gnoemes.shikimori.data.network.CommentApi;
-import com.gnoemes.shikimori.data.network.DocumentVideoApi;
 import com.gnoemes.shikimori.data.network.MangaApi;
 import com.gnoemes.shikimori.data.network.ShimoriVideoApi;
 import com.gnoemes.shikimori.data.network.RanobeApi;
@@ -21,7 +20,7 @@ import dagger.Provides;
 import retrofit2.Retrofit;
 
 @Module(includes = {RetrofitModule.class, CommonNetworkModule.class, VideoNetworkModule.class,
-        AuthCommonNetworkModule.class, DocumentVideoNetworkModule.class})
+        AuthCommonNetworkModule.class})
 public interface ApiModule {
 
     @Singleton
@@ -40,12 +39,6 @@ public interface ApiModule {
     @Provides
     static VideoApi bindVideoApi(@com.gnoemes.shikimori.di.app.annotations.VideoApi Retrofit retrofit) {
         return retrofit.create(VideoApi.class);
-    }
-
-    @Singleton
-    @Provides
-    static DocumentVideoApi bindDocumentVideoApi(@com.gnoemes.shikimori.di.app.annotations.DocumentVideoApi Retrofit retrofit) {
-        return retrofit.create(DocumentVideoApi.class);
     }
 
     @Singleton
