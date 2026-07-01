c5af899596ec8729872c2cebc11e03fb5df5380f
Ali Sha <alernety@gmail.com>
2026-07-01 03:09:36 +0300

    feat(images): batch GraphQL image queries to avoid API rate limiting
    Creates ImageQueryBatcher that coalesces per-entity image queries into single batched queries with comma-separated IDs. Prevents "Retry later" rate limiting from 20+ concurrent individual queries on the calendar tab.
    
    GlideImageLoader refactored to delegate to the batcher instead of firing per-image Apollo queries inline; uses containsKey for cache so failed/null results are persisted and not re-queried.
    
    Fixes: cell-3ehqfq-mr19a3ymobf

--- app/src/main/java/com/gnoemes/shikimori/utils/images/GlideImageLoader.kt
+++ app/src/main/java/com/gnoemes/shikimori/utils/images/GlideImageLoader.kt
@@ -2,28 +2,17 @@ package com.gnoemes.shikimori.utils.images
 
 import android.content.Context
 import android.widget.ImageView
-import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.Optional
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.request.target.BitmapImageViewTarget
 import com.gnoemes.shikimori.R
-import com.gnoemes.shikimori.data.graphql.AnimeByIdQuery
-import com.gnoemes.shikimori.data.graphql.CharacterListQuery
-import com.gnoemes.shikimori.data.graphql.MangaByIdQuery
-import com.gnoemes.shikimori.data.graphql.PersonListQuery
-import com.gnoemes.shikimori.data.graphql.rxQuery
 import com.gnoemes.shikimori.entity.common.domain.Type
-import com.gnoemes.shikimori.utils.appendHostIfNeed
 import com.gnoemes.shikimori.utils.images.blur.BlurTransformation
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class GlideImageLoader @Inject constructor(
         private val context: Context,
-        private val apolloClient: ApolloClient
+        private val batcher: ImageQueryBatcher
 ) : ImageLoader {
 
     companion object {
@@ -86,33 +75,20 @@ class GlideImageLoader @Inject constructor(
 
     private fun resolveAndLoad(image: ImageView, restFallback: String?, entityType: String, entityId: Long) {
         val key = Pair(entityType, entityId)
-        val cached = imageCache[key]
-        if (cached != null) {
-            loadListItemDirect(image, cached)
+        if (imageCache.containsKey(key)) {
+            val cached = imageCache[key]
+            if (cached != null) {
+                loadListItemDirect(image, cached)
+            } else {
+                loadListItemDirect(image, restFallback)
+            }
             return
         }
-        val query = when (entityType) {
-            "anime" -> AnimeByIdQuery(ids = entityId.toString())
-            "manga" -> MangaByIdQuery(ids = entityId.toString())
-            "character" -> CharacterListQuery(ids = Optional.presentIfNotNull(entityId.toString()))
-            "person" -> PersonListQuery(ids = Optional.presentIfNotNull(listOf(entityId.toString())))
-            else -> return
+        batcher.resolve(entityType, entityId) { gqlUrl ->
+            imageCache[key] = gqlUrl
+            val url = gqlUrl ?: restFallback
+            loadListItemDirect(image, url)
         }
-        var disposable: Disposable? = null
-        disposable = apolloClient.rxQuery(query)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ data ->
-                    val gqlUrl = extractImageUrl(entityType, data)
-                    imageCache[key] = gqlUrl
-                    loadListItemDirect(image, gqlUrl ?: restFallback)
-                }, {
-                    imageCache[key] = null
-                    loadListItemDirect(image, restFallback)
-                })
-        val previous = image.getTag(R.id.glide_disposable)
-        if (previous is Disposable) previous.dispose()
-        image.setTag(R.id.glide_disposable, disposable)
     }
 
     private fun loadDirect(image: ImageView, url: String?) {
@@ -133,28 +109,5 @@ class GlideImageLoader @Inject constructor(
                 .load(url)
                 .override(image.measuredWidth / 2, image.measuredHeight / 2)
                 .into(BitmapImageViewTarget(image).apply { waitForLayout() })
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun extractImageUrl(entityType: String, data: Any): String? {
-        return when (entityType) {
-            "anime" -> {
-                val response = data as? AnimeByIdQuery.Data ?: return null
-                response.animes.firstOrNull()?.poster?.originalUrl?.appendHostIfNeed()
-            }
-            "manga" -> {
-                val response = data as? MangaByIdQuery.Data ?: return null
-                response.mangas.firstOrNull()?.poster?.originalUrl?.appendHostIfNeed()
-            }
-            "character" -> {
-                val response = data as? CharacterListQuery.Data ?: return null
-                response.characters.firstOrNull()?.poster?.mainUrl?.appendHostIfNeed()
-            }
-            "person" -> {
-                val response = data as? PersonListQuery.Data ?: return null
-                response.people.firstOrNull()?.poster?.mainUrl?.appendHostIfNeed()
-            }
-            else -> null
-        }
     }
 }
